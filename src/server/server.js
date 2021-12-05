b8b37bd6708beac2a210f0b328a7253d653221dd
petrenko_aleksei <aipgrandfa@yandex.ru>
2021-12-05 23:55:41 +0600

    change URI env

--- src/server/server.js
+++ src/server/server.js
@@ -36,7 +36,7 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth', async (req, res) => {
     axios.post(
         'https://www.reddit.com/api/v1/access_token',
-        `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${URI}`,
+        'grant_type=authorization_code&code='+ req.query.code + '&redirect_uri=' + URI,
         {
             auth: {username: CLIENT_ID, password: PASSWORD},
             headers: {'Content-type': 'application/x-www-form-urlencoded'}
