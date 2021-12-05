746c08ce522e824cec755c588714e2cdc026fb82
petrenko_aleksei <aipgrandfa@yandex.ru>
2021-12-05 20:57:55 +0600

    Added Portal to Loading component Added Procfile for heroku deploy

--- src/shared/Components/ReusedComponents/Loading/Loading.tsx
+++ src/shared/Components/ReusedComponents/Loading/Loading.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './loading.scss';
+import ReactDOM from "react-dom";
 
 export function Loading() {
-    return (
+    const [node, setNode] = useState<Element>();
+    useEffect(() => {
+        setNode(document.querySelector('#modal__root') ?? undefined);
+    }, [])
+    if (!node) return null;
+    return ReactDOM.createPortal((
         <div className={styles.container}>
             <div className={styles.loader}>
                 <div className={styles.text}>Loading</div>
@@ -14,5 +20,6 @@ export function Loading() {
                 </div>
             </div>
         </div>
+        ), node
     );
 }
