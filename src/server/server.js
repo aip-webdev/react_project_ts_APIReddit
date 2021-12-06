075cacd1c23d228a61624ace94f105564bb32834
petrenko_aleksei <aipgrandfa@yandex.ru>
2021-12-07 01:30:34 +0600

    change userBlockComponent

--- src/server/server.js
+++ src/server/server.js
@@ -33,7 +33,7 @@ const reqHandler = async (req, res) => {
 
 app.use('/static', express.static('./dist/client'));
 app.use('/img-src', express.static('./dist/img-src'));
-app.get('/auth', (req, res) => {
+app.get('/auth', async (req, res) => {
     axios.post(
         'https://www.reddit.com/api/v1/access_token',
         'grant_type=authorization_code&code='+ req.query.code + '&redirect_uri=' + URI + '/auth',
