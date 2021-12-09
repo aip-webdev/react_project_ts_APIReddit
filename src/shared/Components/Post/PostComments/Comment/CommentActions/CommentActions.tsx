213a60936e18e8005fc89f1df6b1341d8ba1cb24
petrenko_aleksei <aipgrandfa@yandex.ru>
2021-12-10 01:31:37 +0600

    Change Post, Modal styles for mobile layout

--- src/shared/Components/Post/PostComments/Comment/CommentActions/CommentActions.tsx
+++ src/shared/Components/Post/PostComments/Comment/CommentActions/CommentActions.tsx
@@ -3,6 +3,7 @@ import styles from './commentactions.scss';
 import {Comments} from "../../../../CardList/Card/Controls/Comments";
 import {Share} from "../../../../CardList/Card/Controls/Actions/Share";
 import {Complain} from "../../../../CardList/Card/Controls/Actions/Complain";
+import {useMediaSize} from "../../../../../../hooks/useMediaSize";
 
 
 interface ICommentActionsProps {
@@ -10,16 +11,17 @@ interface ICommentActionsProps {
 }
 
 export function CommentActions({handleClick}: ICommentActionsProps) {
+    const [isMediaSize,] = useMediaSize();
     return (
         <div className={styles.postActions}>
             <Comments
                 onclick={handleClick}
-                textHide={false}
+                textHide={isMediaSize}
                 text='Ответить'
                 btnClassName={styles.actionButton}
             />
-            <Share textHide={false} btnClassName={styles.actionButton}/>
-            <Complain textHide={false} btnClassName={styles.actionButton}/>
+            <Share textHide={isMediaSize} btnClassName={styles.actionButton}/>
+            <Complain textHide={isMediaSize} btnClassName={styles.actionButton}/>
         </div>
     );
 }
