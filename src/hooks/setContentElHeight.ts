b7b0ada62784c20a145093a99b9f9206c06ea208
petrenko_aleksei <aipgrandfa@yandex.ru>
2021-12-12 00:35:30 +0600

    Change Post methods and styles

--- src/hooks/setContentElHeight.ts
+++ src/hooks/setContentElHeight.ts
@@ -5,7 +5,7 @@ import {useMediaSize} from "./useMediaSize";
 export function setContentElHeight() {
     const [isMediaMobile, isMediaTablet, isMediaDesktop] = useMediaSize();
     /*Введем погрешность для разных экранов, чтобы scroll всегда срабатывал*/
-    let errorRate = useMemo(() => isMediaTablet ? 12 : (isMediaDesktop ? 18 : 0), [isMediaDesktop, isMediaTablet, isMediaMobile]);
+    let errorRate = useMemo(() => isMediaTablet ? 10 : (isMediaDesktop ? 0 : -5), [isMediaDesktop, isMediaTablet, isMediaMobile]);
     let height = useBodyHeight();
 
     useEffect(() => {
@@ -13,7 +13,7 @@ export function setContentElHeight() {
         let contentEl = window.document.getElementById('js-content');
         if (!headerEl || !contentEl) return;
 
-        let contentHeight = 100 - Math.ceil(headerEl.offsetHeight / (height / 100));
+        let contentHeight = 100 - Math.ceil(headerEl.offsetHeight / (height / 100)) - errorRate;
         contentEl.style.height = `${contentHeight}vh`;
 
     }, [height, errorRate]);
